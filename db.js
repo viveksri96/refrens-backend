c1b11e8d8be5e426b6f0a0108c5ffcf9a7c28009
Vivek Srivastava <vivsri95@gmail.com>
2021-01-18 05:25:17 +0530

    fix: updated cluster region

--- db.js
+++ db.js
@@ -1,21 +1,22 @@
 const { MongoClient, ObjectID } = require("mongodb");
 
-const client = new MongoClient("mongodb+srv://admin:admin@cluster0.9v7hr.mongodb.net/test-refrens?retryWrites=true&w=majority");
+const client = new MongoClient(
+  "mongodb+srv://admin:admin@cluster0.4tobc.mongodb.net/refrens?retryWrites=true&w=majority"
+);
 
+async function connectClient() {
+  try {
+    await client.connect({
+      useUnifiedTopology: true,
+    });
 
-async function connectClient(){
-    try {
-        await client.connect({
-          useUnifiedTopology: true
-        });
-        
-        console.log('db connectd')
-    } catch (e) {
-        console.error(e);
-    }
+    console.log("db connectd");
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 module.exports = {
-    client,
-    connectClient
-}
+  client,
+  connectClient,
+};
